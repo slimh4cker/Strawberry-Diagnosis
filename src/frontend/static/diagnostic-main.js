7f0d104830c9d5d05a1204bdc002413b1fda7f04
Andres Barragan <al22760549@ite.edu.mx>
2025-11-30 18:25:46 -0800

    llamada a la api real incorporada

--- src/frontend/static/diagnostic-main.js
+++ src/frontend/static/diagnostic-main.js
@@ -1,11 +1,21 @@
 import { processDiagnosticSource } from "./DiagnosticoCard.js";
-import { get_dummy } from "./getDiagnostic.js";
+import { post_diagnostic } from "./getDiagnostic.js";
 // Si luego quieren usar la API real:
 // import { post_diagnostic } from "./getDiagnostic.js";
 
+// TODO alert si se seleccionan 1 o menos sintomas
 document.getElementById("btn-diagnosticar").addEventListener("click", async () => {
+    // Los sintomas seleccionados están en la variable global window.sintomasSeleccionados
 
-    const data = await get_dummy();   // usa lo que ya existía
+    // se eliminan los datos no necesarios para el backend
+    // Los sintomas se almacenan como {"hecho": "sintoma_hoja", "valor": "danada", "nombre": "dañada", parte: "hoja"}
+    // solo nos quedamos con hehco y valor
+    const seleccionados = window.sintomasSeleccionados.map(s => {
+        return {"hecho": s.hecho, "valor": s.valor};
+    });
+
+    const data = await post_diagnostic(seleccionados);   // usa lo que ya existía
     // luego se cambiara por const data = await post_diagnostic(seleccionados);
+
     processDiagnosticSource(data);    // muestra la tarjeta
 });
